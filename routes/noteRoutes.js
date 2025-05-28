beeccb30b74c4311f0eb867bf979508d77181a9d
Developer-Waqas <waqas.developer5254@gmail.com>
2025-05-29 02:12:32 +0500

    ✨ First Commit

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -9,19 +9,14 @@ const auth = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-// All routes require authentication
 router.use(auth);
 
-// GET /api/notes/all - Get all notes for the user
 router.get('/all', getNotes);
 
-// POST /api/notes/create - Create a new note
 router.post('/create', createNote);
 
-// PUT /api/notes/update/:id - Update a note
 router.put('/update/:id', updateNote);
 
-// DELETE /api/notes/delete/:id - Delete a note
 router.delete('/delete/:id', deleteNote);
 
 module.exports = router;
